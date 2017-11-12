d5ea444de4e604330d2b30ab1c1c716ff03d4526
ellonde <mnbaltzersen@gmail.com>
2017-11-12 22:07:49 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,3 @@
 export CUDA_VISIBLE_DEVICES="1"
 
-#if [ -d "log_root" ] && [ "$1" == "-r" ]; then
-# echo "removing logdir"
-# rm -r log_root
-#fi
-
-python train.py /nobackup/titans/s155992/Warwick_Dataset
+python train.py dataset
